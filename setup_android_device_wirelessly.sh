3ac12c402aa609377671b62774246df547200f71
ROHIT <100354219+rohit-krish@users.noreply.github.com>
2022-06-01 19:49:06 +0530

    Create setup_android_device_wirelessly.sh

---
+++ setup_android_device_wirelessly.sh
@@ -0,0 +1,17 @@
+# Script 1
+#! /bin/bash
+
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+SEC=5
+
+adb tcpip 5555
+
+while [ $SEC -ge 1 ];do
+    echo -ne "${RED}WAITING TO AGREE PERMISSION ...($SEC)\033[0K\r${NC}"
+    let "SEC=SEC-1"
+    sleep 1
+done
+
+adb connect $1:5555
+flutter devices
